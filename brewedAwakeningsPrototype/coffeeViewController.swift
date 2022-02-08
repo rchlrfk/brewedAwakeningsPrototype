a5b3af8f60c2941cc756b893e67ab1b058cfaff4
Rachel Rafik <rafikrachel@gmail.com>
2022-02-08 11:19:51 -0600

    when image pressed, it's added to a table view and is able to be deleted

--- brewedAwakeningsPrototype/coffeeViewController.swift
+++ brewedAwakeningsPrototype/coffeeViewController.swift
@@ -7,23 +7,49 @@
 
 import UIKit
 
-class coffeeViewController: UIViewController {
-
+class coffeeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var teaImageView: UIImageView!
+    @IBOutlet weak var flavorImageView: UIImageView!
+    
+    @IBOutlet weak var tableView: UITableView!
+    var items:[String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+            imageView.isUserInteractionEnabled = true
+            imageView.addGestureRecognizer(tapGestureRecognizer)
+            
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
+    {
+        let newItem = "coffee"
+        items.append(newItem)
+        tableView.reloadData()
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            items.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.automatic)
+        }
+    }
 
 }
