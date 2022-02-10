129db3ed02fb22ee0ad2f0c06480aba04cd0e04a
Rachel Rafik <rafikrachel@gmail.com>
2022-02-10 11:01:34 -0600

    made outlet collection and trying to learn how to loop through each image to create a tap gesture

--- brewedAwakeningsPrototype/coffeeViewController.swift
+++ brewedAwakeningsPrototype/coffeeViewController.swift
@@ -7,46 +7,54 @@
 
 import UIKit
 
-class coffeeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class coffeeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
+{
     
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var teaImageView: UIImageView!
-    @IBOutlet weak var flavorImageView: UIImageView!
+    @IBOutlet var images: [UIImageView]!
     
     @IBOutlet weak var tableView: UITableView!
     var items:[String] = []
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
-            imageView.isUserInteractionEnabled = true
-            imageView.addGestureRecognizer(tapGestureRecognizer)
-            
-        // Do any additional setup after loading the view.
+
     }
     
-    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
+    @IBAction func whenTapGesturePressed(_ sender: UITapGestureRecognizer)
     {
-        let newItem = "coffee"
-        items.append(newItem)
-        tableView.reloadData()
+        let selectedPoint = sender.location(in: view)
+        print(selectedPoint)
+        for img in images
+        {
+            if img.frame.contains(selectedPoint)
+            {
+                let newItem = "coffee"
+                items.append(newItem)
+                tableView.reloadData()
+            }
+        }
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
         return items.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
         cell.textLabel?.text = items[indexPath.row]
         return cell
     }
     
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == UITableViewCell.EditingStyle.delete {
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == UITableViewCell.EditingStyle.delete
+        {
             items.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.automatic)
         }
